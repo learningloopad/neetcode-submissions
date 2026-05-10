824837ebcc763eaf580c27006a5c63b4ddcf8bb1
tungpham <tungps.hust@gmail.com>
2026-05-10 18:05:16 +0700

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let res = ''
+
+        for (let str of strs) {
+            const length = str.length
+
+            res += `${length}#${str}`
+        }
+        console.log({res})
+        return res
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let i = 0
+        let res = []
+
+        while (i < str.length) {
+            for (let j = i; j < str.length; j++) {
+                if (str[j] === '#') {
+                    const length = Number(str.slice(i, j))
+                    // console.log({length})
+
+                    const word = str.slice(j + 1, j + length + 1)
+                    // console.log({word})
+                    res.push(word)
+                    i = j + length + 1
+                    break
+                }
+            }
+        }
+
+        return res
+    }
+}
