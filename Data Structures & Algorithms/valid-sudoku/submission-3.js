cfe1acdfa26b8d84a314826ce6e92f1d659ff1dd
tungpham <tungps.hust@gmail.com>
2026-05-12 13:49:36 +0700

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        const rows = Array.from({ length: 9 }, () => new Set())
+        const columns = Array.from({ length: 9 }, () => new Set())
+        const blocks = new Map()
+
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                const blockKey = `${Math.floor(Number(i)/3)}-${Math.floor(Number(j)/3)}`
+                const block = blocks.get(blockKey)
+
+                if (rows[i].has(board[i][j]) || columns[j].has(board[i][j]) || (block && block.has(board[i][j]))) {
+                    return false
+                }
+
+                if (board[i][j] !== '.') {
+                    rows[i].add(board[i][j])
+                    columns[j].add(board[i][j])
+
+                    if (!block) {
+                        blocks.set(blockKey, new Set([board[i][j]]))
+                    } else {
+                        blocks.set(blockKey, block.add(board[i][j]))
+                    }
+                }
+
+            }
+        }
+
+        return true
+    }
+}
