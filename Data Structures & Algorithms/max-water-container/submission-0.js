04ba3545be059773861e2a31652eb68150ca69cb
tungpham <tungps.hust@gmail.com>
2026-05-15 10:24:36 +0700

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let l = 0
+        let r = heights.length - 1
+        let max = 0
+
+        while (l < r) {
+            const water = Math.min(heights[l], heights[r])*(r - l)
+
+            max = Math.max(max, water)
+
+            if (heights[r] > heights[l]) {
+                l++
+            } else {
+                r--
+            }
+        }
+
+        return max
+    }
+}
